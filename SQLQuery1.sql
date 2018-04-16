c26bb6888a30c15993bbe5fa509cb254ff2d2330
brunooliv <brunooliveira.xp@gmail.com>
2018-04-16 14:40:09 -0300

    Agrupando dados e fazendo consultas mais inteligentes

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,23 +1,6 @@
 use controle_compras;
 
-select * from compras;
-
-insert into compras values (150, '2015-04-15', 'churraqueira',  1);
-
-insert into compras (valor, data, recebida) values (150, '2015-04-15', 1);
-
-delete from compras where observacoes is null;
-
-select * from compras where observacoes is null;
-
-alter table compras alter column observacoes nvarchar(200) not null;
-
-
-alter table compras add default '0' for recebida;
-
-insert into compras (valor, data, observacoes) values (200, '2015-10-04', 'testando default');
-
-select * from compras where observacoes = 'testando default';
-
-
-sp_help compras;
+select month(data) as mes, year(data) as ano, recebida, sum(valor) as soma from compras
+where valor < 1000
+group by recebida, month(data), year(data)
+order by year(data) 
